d1b41c2a9bf1b089c03dc70fe67ff4eb9b9f74ef
Simone Covelli <simone.covelli03@gmail.com>
2025-11-12 12:01:50 +0100

    resolve initialization problem

--- src/llm_interaction/RealtimeInteraction.ts
+++ src/llm_interaction/RealtimeInteraction.ts
@@ -103,6 +103,8 @@ export class RealtimeInteraction {
         if (!(await this.setupLocalAudio())) return;
         if (!this.setupDataChannel()) return;
         await this.connectToModel();
+
+        this.resetLastCoords();
     }
 
     private stopSession(): void {
@@ -134,6 +136,14 @@ export class RealtimeInteraction {
         console.log("Session closed");
         this.handleSessionState(false);
         this.handleAudioState(false);
+    }
+
+    // ------------------
+    // RESET COORDINATES
+    // ------------------
+
+    private resetLastCoords(): void {
+        this.lastCoords = { lastX: 100000, lastY: 100000 }; // 100000 are only placeholders
     }
 
     // ------------
